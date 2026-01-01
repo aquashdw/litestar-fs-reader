ba9c3545e7a24106485c1a18521dc702a5b2d40b
aquashdw <aquashdw@gmail.com>
2026-01-01 23:57:01 +0900

    nested path

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
 import os
 from pathlib import Path
 
-from typing import List
-from litestar import Litestar, get
+from typing import List, Union, Literal
+from litestar import Litestar, get, Request
 from dotenv import load_dotenv
 
 load_dotenv()
 ROOT_DIR = os.environ.get('ROOT_DIR', '.')
 
 @get('/')
-def index() -> List[str]:
+async def index() -> List[str]:
     return [str(item) for item in Path(ROOT_DIR).iterdir()]
 
 
-app = Litestar([index])
+@get('/{full_path:path}')
+async def get_path(
+        request: Request,
+        full_path: str,
+        target: Union[Literal['dir'] | Literal['file']] = 'file') -> List[str]:
+    return [str(item) for item in Path(ROOT_DIR).iterdir()]
+
+
+app = Litestar([index, get_path])
