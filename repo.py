e6a5848174ee1ade308fc4903f64462e00f28bb4
aquashdw <aquashdw@gmail.com>
2026-05-01 15:39:44 +0900

    rollback on exception

--- repo.py
+++ repo.py
@@ -28,6 +28,8 @@ class Repository:
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type is not None:
+            self.session.rollback()
         self.session.commit()
         self.session.close()
         self.session = None
