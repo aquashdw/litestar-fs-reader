23f376453a35a18fdac8651ecf19f49c6e40c7a7
aquashdw <aquashdw@gmail.com>
2026-05-05 21:05:36 +0900

    use new repo pattern in init script

--- init.py
+++ init.py
@@ -2,7 +2,6 @@ from pathlib import Path
 
 from models import FSObject, Base, FSObjectType
 from repo import RepositoryFactory, FSRepository
-from repo_bak import FSRepository as repo_before
 
 repo_factory = RepositoryFactory()
 
@@ -27,48 +26,44 @@ def check_schema(connection_string: str = 'sqlite:///test.sqlite'):
             session.create_root()
 
 
-def compare_fs_db(root_dir: str = '.', connection_string: str = 'sqlite:///test.sqlite'):
+def compare_fs_db(root_dir: str = '.root', connection_string: str = 'sqlite:///test.sqlite'):
     """
     starting with root_dir, compare database records with the actual filesystem.
     """
     root_path = Path(root_dir).absolute()
-    repo = repo_before(connection_string)
-    with repo() as session:
+    with repo_factory(FSRepository) as session:
         root_entity = session.get_by_path('/')
 
-        def check_dir(path: Path, parent_dto: FSObject):
+        def check_dir(path: Path, cwd: FSObject):
             if path.is_file():
                 return
-
             fs_summary = {}
             for obj in path.iterdir():
                 fs_summary[obj.name] = {
                     'path_obj': obj,
                     'type': FSObjectType.FILE if obj.is_file() else FSObjectType.DIR,
                 }
-            print(len(fs_summary))
+                print(len(fs_summary))
 
-            fd_db_diff = []
-            inner_dirs = []
-            for child in repo.listdir(parent_dto.id):
+                fd_db_diff = []
+                inner_dirs = []
+                for child in session.listdir(cwd):
+                    if child.name not in fs_summary:
+                        # TODO remove file from db, because it was removed in fs
+                        continue
+                    else:
+                        file = fs_summary.pop(child.name)
+                    if child.type != file['type']:
+                        child.type = file['type']
+                        fd_db_diff.append(child)
 
-                # TODO remove file not in filesystem
-                if child.name not in fs_summary:
-                    continue
-                else:
-                    file = fs_summary.pop(child.name)
-                if child.type != file['type']:
-                    child.type = file['type']
-                    fd_db_diff.append(child)
-                    # TODO alter if file dir mismatch
-                    pass
+                    if file['type'] == FSObjectType.DIR:
+                        inner_dirs.append((file['path_obj'], child))
+                    # TODO update db to match filesystem
 
-                if file['type'] == FSObjectType.DIR:
-                    inner_dirs.append((file['path_obj'], child))
-
-            print(len(fs_summary))
-            for inner_dir in inner_dirs:
-                check_dir(*inner_dir)
+                print(len(fs_summary))
+                for inner_dir in inner_dirs:
+                    check_dir(*inner_dir)
 
         check_dir(root_path, root_entity)
 
